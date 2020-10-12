fa8e06102bf2c211933a25d780553b0fe86586c2
Roman Segador <romansegador@gmail.com>
2020-10-12 12:08:08 +0200

    prepare for workshop

--- publish-pacts.js
+++ publish-pacts.js
@@ -5,11 +5,34 @@
 
 let projectFolder = __dirname;
 let pact = require('@pact-foundation/pact-node');
+let consumerVersion = require('child_process')
+  .execSync('git rev-parse HEAD')
+  .toString().trim();
+let pactBroker = 'http://ip172-18-0-32-bu22dl7p2ffg00blhnvg-9292.direct.labs.play-with-docker.com/';
+
+if(process.argv.length === 4) {
+  consumerVersion = process.argv[2];
+  pactBroker = process.argv[3];
+}
+let folders = [];
+const fs = require("fs"); // Or `import fs from "fs";` with ESM
+
+if (fs.existsSync(projectFolder + '/pacts')) {
+  folders.push(projectFolder + '/pacts');
+}
+
+if (fs.existsSync(projectFolder + '/apps/web-app/pacts')) {
+  folders.push(projectFolder + '/apps/web-app/pacts');
+}
+
+console.log('pact broker: ' + pactBroker);
+console.log('Consumer version: ' + consumerVersion);
+console.log('pact locations: ' + folders);
 
 let options = {
-  pactFilesOrDirs: [projectFolder + '/pacts'],
-  pactBroker: process.argv[3],
-  consumerVersion: process.argv[2],
+  pactFilesOrDirs: folders,
+  pactBroker: pactBroker,
+  consumerVersion: consumerVersion,
   tags: ['build']
 };
 
